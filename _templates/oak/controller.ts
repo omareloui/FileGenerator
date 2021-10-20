f46550596de739e800eddbe232dc313021e2b226
Omar Hussein <omarelwy@hotmail.com>
2021-10-20 17:08:37 +0200

    🔥 Remove all personal config and template files and add _templates to gitignore

--- _templates/oak/controller.ts
+++
@@ -1,6 +0,0 @@
-import type { RouterContext } from "../deps.ts";
-import { <%= kebabToPascal(name) %>Service } from "../services/index.ts";
-
-export class <%= kebabToPascal(name) %>Controller {
-  public static async create({ request, response, state }: RouterContext) {}
-}
--- config/templates.ts
+++ config/templates.ts
@@ -1,38 +1,5 @@
-import { TemplateConfig, TemplateProps } from "../types.ts";
+import { TemplateConfig } from "../types.ts";
 
-const LICENCE_PROPS: TemplateProps = {
-  name: {
-    isRequired: true,
-  },
-  email: {
-    isRequired: false,
-  },
-  date: {
-    default: new Date().getFullYear(),
-    shouldAsk: false,
-  },
-};
-
-const LICENCE_NAME = "LICENCE";
-
-const REQUIRED_KEBAB = {
-  isRequired: true,
-  hint: "in kebab-case",
-};
-
-const REQUIRED_KEBAB_NAME = {
-  name: REQUIRED_KEBAB,
-};
-
-const templatesConfig: TemplateConfig = {
-  lit: { props: { name: REQUIRED_KEBAB }, defaultFilename: "{name}" },
-  "mongoose-model": { props: REQUIRED_KEBAB_NAME, defaultFilename: "{name,p}" },
-  "oak-controller": {
-    props: REQUIRED_KEBAB_NAME,
-    defaultFilename: "{name}.controller",
-  },
-  "licence-wtfpl": { props: LICENCE_PROPS, defaultFilename: LICENCE_NAME },
-  "licence-mit": { props: LICENCE_PROPS, defaultFilename: LICENCE_NAME },
-};
+const templatesConfig: TemplateConfig = {};
 
 export default templatesConfig;
