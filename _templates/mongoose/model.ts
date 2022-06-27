e68694356230fc954e0b5ecf658d27936e125075
Omar Hussein <omarelwy@hotmail.com>
2022-06-27 19:23:51 +0200

    ✨ Setting up the node project

--- _templates/mongoose/model.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const Schema = mongoose.Schema;
-
-const <%= kebabToPascal(name) %>Schema = new Schema({});
-
-export default mongoose.model("<%= name %>", <%= kebabToPascal(name) %>Schema);
--- utils/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./collectData.ts";
-export * from "./createFromTemplate.ts";
-export * from "./files.ts";
-export * from "./ask.ts";
-export * from "./templates.ts";
-export * from "./caseConverts.ts";
-export * from "./customSyntax.ts";
